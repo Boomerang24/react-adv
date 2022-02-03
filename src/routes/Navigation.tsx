447006b5a4a8e5de95ff6751e1fd653d72eaaa32
Alexis Rico <alexcomoed@gmail.com>
2022-02-02 21:16:29 -0600

    Paginas lazy agregadas

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,34 +1,41 @@
-import { BrowserRouter, Navigate, NavLink, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
+import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+
 import logo from '../logo.svg';
 
 export const Navigation = () => {
-    return (
-        <BrowserRouter>
-            <div className="main-layout">
-                <nav>
-                    <img src={ logo } alt="React Logo" />
-                    <ul>
-                        <li>
-                           <NavLink to="/home" className={ ({ isActive }) => isActive ? 'nav-active' : ''}>Home</NavLink>  
-                        </li>
-                        <li>
-                           <NavLink to="/about" className={ ({ isActive }) => isActive ? 'nav-active' : ''}>About</NavLink>  
-                        </li>
-                        <li>
-                           <NavLink to="/users" className={ ({ isActive }) => isActive ? 'nav-active' : ''}>Users</NavLink>  
-                        </li>
-                    </ul>
-                </nav>
+  return (
+    <Router>
+      <div className="main-layout">
+        <nav>
+            <img src={ logo } alt="React Logo" />
+          <ul>
+            <li>
+              <NavLink to="/lazy1" activeClassName="nav-active" exact>Lazy1</NavLink>
+            </li>
+            <li>
+              <NavLink to="/lazy2" activeClassName="nav-active" exact>Lazy2</NavLink>
+            </li>
+            <li>
+              <NavLink to="/lazy3" activeClassName="nav-active" exact>Lazy3</NavLink>
+            </li>
+          </ul>
+        </nav>
 
-                <Routes>
-                    <Route path="about" element={ <h1>About Page</h1> }/>
-                    <Route path="users" element={ <h1>Users Page</h1> }/>
-                    <Route path="/home" element={ <h1>Home Page</h1> }/>
-
-                    {/* //? Usado para hacer un Redirect */}
-                    <Route path="/*" element={ <Navigate to="/home" replace /> }/>
-                </Routes>
-            </div>
-        </BrowserRouter>
-    )
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/lazy1">
+            <LazyPage1 />
+          </Route>
+          <Route path="/lazy2">
+            <LazyPage2 />
+          </Route>
+          <Route path="/lazy3">
+            <LazyPage3 />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
