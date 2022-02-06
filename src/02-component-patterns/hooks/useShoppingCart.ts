225693fd33328b035c82e3da56ea352234a5c3f2
Alexis Rico <alexcomoed@gmail.com>
2022-02-05 18:57:35 -0600

    Control props alt

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -14,32 +14,16 @@ export const useShoppingCart = () => {
 
         setShoppingCart( oldShoppingCart => {
 
-            //* Añadir producto
-            const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 }; //Se busca el product que cumpla con ese id, si es null, devuelve el 2do objeto
-            if( Math.max( productInCart.count + count, 0) > 0 ){ // Se busca el valor mas alto, limitandolo a 0
-                productInCart.count += count;
-                return {
-                    ...oldShoppingCart,
-                    [product.id]: productInCart
-                }
+            if( count === 0 ){
+                const { [product.id]: toDelete, ...rest } = oldShoppingCart; // Se desestructura la propiedad a eliminar
+                // console.log({ toDelete });
+                return { ...rest }; // Se retorna lo demas, dejando fuera el producto
             }
 
-            //* Borrar producto
-            const { [product.id]: toDelete, ...rest } = oldShoppingCart; // Se desestructura la propiedad a eliminar
-            return { ...rest }; // Se retorna lo demas, dejando fuera el producto
-
-
-            //? Implementacion sencilla
-            // if( count === 0 ){
-            //     const { [product.id]: toDelete, ...rest } = oldShoppingCart; // Se desestructura la propiedad a eliminar
-            //     // console.log({ toDelete });
-            //     return { ...rest }; // Se retorna lo demas, dejando fuera el producto
-            // }
-
-            // return {
-            //     ...oldShoppingCart,
-            //     [ product.id ]: { ...product, count }
-            // }
+            return {
+                ...oldShoppingCart,
+                [ product.id ]: { ...product, count }
+            }
         });
     };
 
